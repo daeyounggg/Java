3ccca5350e313293513a83509959c63dc3fc52fd
daeyounggg <kdyzab@gmail.com>
2023-09-07 16:58:18 +0900

    객체

---
+++ 4. 프로그래밍 언어 응용/java_study/day03/src/exam02/Ex02.java
@@ -0,0 +1,15 @@
+package exam02;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        int result = add(10,20);
+        System.out.println(result);
+    }
+    
+    static int add(int num1, int num2){
+
+        int result = num1 + num2;
+
+        return result;
+    }
+}
