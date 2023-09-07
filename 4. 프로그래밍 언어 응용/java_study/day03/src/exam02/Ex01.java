1e4ff4a206305e82b763e775179c8faf526b146e
daeyounggg <kdyzab@gmail.com>
2023-09-07 17:51:16 +0900

    객체

--- 4. 프로그래밍 언어 응용/java_study/day03/src/exam02/Ex01.java
+++ 4. 프로그래밍 언어 응용/java_study/day03/src/exam02/Ex01.java
@@ -8,5 +8,22 @@ public class Ex01 {
         s1.subject="과목1";
 
         s1.study();
+        System.out.println(System.identityHashCode(s1));
+
+        Student s2 = new Student();
+        s2.id = 1001;
+        s2.name = "학생2";
+        s2.subject = "과목2";
+
+        s2.study();
+        System.out.println(System.identityHashCode(s2));
+
+        Student s3 = s2; // s2의 주소값
+        s3.name = "학생3";
+        s3.study();
+        s2.study();
+        System.out.println(System.identityHashCode(s3));
+        System.out.println(s2 == s3);
+
     }
 }
