7d3178be2ce97d1e0364611ee3c1c8cd22756f72
daeyounggg <kdyzab@gmail.com>
2023-09-07 15:10:16 +0900

    자바 배열

---
+++ 4. 프로그래밍 언어 응용/java_study/day03/src/exam01/Ex05.java
@@ -0,0 +1,13 @@
+package exam01;
+
+public class Ex05 {
+    public static void main(String[] args) {
+        int[] nums = { 10, 20, 30, 40, 50, 60};
+        System.out.println(nums.length); // 공간의 갯수
+
+        for(int i = 0; i < nums.length; i++){
+            System.out.println(nums[i]);
+        }
+
+    }
+}
