826ac5a2327b8f5e0c6a797212f476e67bb5f295
daeyounggg <kdyzab@gmail.com>
2023-09-08 16:19:00 +0900

    공부 자료

---
+++ 4. 프로그래밍 언어 응용/java_study/day04/src/exam02/Ex02.java
@@ -0,0 +1,16 @@
+package exam02;
+
+import exam03.*;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        A a = new A();
+        // a.num1;
+        System.out.println(a.num2);
+        a.print();
+        // a.num4
+
+        B b = new B();
+        b.print2();
+    }
+}
