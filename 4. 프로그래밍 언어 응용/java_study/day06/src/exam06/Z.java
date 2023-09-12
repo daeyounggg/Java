ec4c03b72c5d10671826a238ea332e309616484d
daeyounggg <kdyzab@gmail.com>
2023-09-12 17:03:48 +0900

    자바

--- 4. 프로그래밍 언어 응용/java_study/day06/src/exam06/Z.java
+++ 4. 프로그래밍 언어 응용/java_study/day06/src/exam06/Z.java
@@ -1,4 +1,5 @@
 package exam06;
 
-public interface Z {
+public interface Z extends X, Y {
+    void method3();
 }
---
+++ 4. 프로그래밍 언어 응용/java_study/day06/src/exam07/Outer.java
@@ -0,0 +1,9 @@
+package exam07;
+
+public class Outer {
+    class Inner {
+        void method(){
+            System.out.println("인스턴스 내부 클래스!");
+        }
+    }
+}
