061be39ef1f5cbf2ff10f5744f138afbb1a5ff3e
daeyounggg <kdyzab@gmail.com>
2023-09-13 15:12:40 +0900

    자바

---
+++ 4. 프로그래밍 언어 응용/java_study/day07/src/exam01/Outer.java
@@ -0,0 +1,33 @@
+package exam01;
+
+public class Outer {
+
+    public Calculator cal = new Calculator() {
+
+        public int add(int num1, int num2) {
+            return num1 + num2;
+        }
+    };
+
+    Calculator method(int num3) { // final int num3
+        return new Calculator() {
+            public int add(int num1, int num2){
+                // num3 =40;
+                return num1 + num2 + num3;
+            }
+        };
+    }
+
+    /*
+    void method(){
+        class Inner{
+            void method(){
+                System.out.println("지역 내부 클래스!");
+            }
+        }
+
+        Inner in = new Inner();
+        in.method();
+    }
+     */
+}
