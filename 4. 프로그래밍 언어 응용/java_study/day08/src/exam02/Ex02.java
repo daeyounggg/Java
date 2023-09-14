957cb98b6c31d9fc07a6e9f747870ed7d431fc90
daeyounggg <kdyzab@gmail.com>
2023-09-14 16:42:49 +0900

    자바

--- 4. 프로그래밍 언어 응용/java_study/day08/src/exam02/Ex02.java
+++ 4. 프로그래밍 언어 응용/java_study/day08/src/exam02/Ex02.java
@@ -2,10 +2,16 @@ package exam02;
 
 public class Ex02 {
     public static void main(String[] args) {
+        /*
         Human human = new Human();
         Tiger tiger = new Tiger();
         Bird bird = new Bird();
-
-        Object[] animals = { human, tiger, bird };
+        */
+       // Object[] animals = { human, tiger, bird };
+        Object[] animals = {
+                new Human(),
+                new Bird(),
+                new Tiger()
+        };
     }
 }
