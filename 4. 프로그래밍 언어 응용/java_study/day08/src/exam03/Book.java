4590bde294a14bf58442ad59f070756c06be60db
daeyounggg <kdyzab@gmail.com>
2023-09-15 14:43:41 +0900

    자바

--- 4. 프로그래밍 언어 응용/java_study/day08/src/exam03/Book.java
+++ 4. 프로그래밍 언어 응용/java_study/day08/src/exam03/Book.java
@@ -1,5 +1,7 @@
 package exam03;
 
+import java.util.Objects;
+
 public class Book {
     private String title;
     private String author;
@@ -41,6 +43,20 @@ public class Book {
                 "title='" + title + '\'' +
                 ", author='" + author + '\'' +
                 ", publisher='" + publisher + '\'' +
+                ", hashCode()='" + hashCode() + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(title, book.title) && Objects.equals(author, book.author) && Objects.equals(publisher, book.publisher);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, publisher);
     }
 }
