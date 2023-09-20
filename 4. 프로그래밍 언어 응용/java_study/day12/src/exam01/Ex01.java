f7deaa747182f8055e141128a74eb1efebaba22f
daeyounggg <kdyzab@gmail.com>
2023-09-20 17:13:14 +0900

    자바

---
+++ 4. 프로그래밍 언어 응용/java_study/day12/src/exam01/Ex01.java
@@ -0,0 +1,35 @@
+package exam01;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("이름1");
+        names.add("이름2");
+        names.add("이름3");
+        names.add("이름4");
+        names.add("이름5");
+
+        LinkedList<String> names2 = new LinkedList<>(names);
+
+        /*
+        for (int i = 0; i < names.size(); i++) {
+            names.remove(i);
+        }
+
+
+        for(int i = names.size() - 1; i >= 0; i--){
+            names.remove(i);
+        }
+         */
+
+        for (int i = 0; i < names.size(); i++) {
+            String name = names.get(i);
+            System.out.println(name);
+        }
+
+    }
+}
