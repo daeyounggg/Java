9e12f4c0df3b7c5f9288fbc8fb2269f48f4f8662
daeyounggg <kdyzab@gmail.com>
2023-09-26 14:50:36 +0900

    스트림

---
+++ 4. 프로그래밍 언어 응용/java_study/day16/src/exam01/Ex01.java
@@ -0,0 +1,37 @@
+package exam01;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toCollection;
+
+public class Ex01 {
+
+    private static List<Student> students = new ArrayList<>();
+    public static void main(String[] args) {
+
+        IntStream.rangeClosed(1,20).forEach(Ex01::add);
+
+        LinkedList<String> names = students.stream().map(Student::getName)
+                .collect(toCollection(LinkedList::new));
+
+        System.out.println(names);
+
+        /*
+        Set<Integer> bans = students.stream()
+                .map(Student::getBan)
+                //.distinct()
+                .collect(Collectors.toSet());
+        System.out.println(bans);
+
+         */
+    }
+    private static void add(int i) {
+        int grade = (int)(Math.random() * 3) + 1;
+        int ban = (int)(Math.random() * 3) + 1;
+        char gender = (new Random()).nextBoolean() ? 'F':'M';
+
+        students.add(new Student(grade, ban, gender,"학생" + i));
+    }
+}
