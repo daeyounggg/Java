2a761d8f91db24bf2061858af824e35632746755
daeyounggg <kdyzab@gmail.com>
2023-10-11 16:19:18 +0900

    TDD

--- 5. 프로그래밍 언어 활용/web_study/day02/src/test/java/models/member/LoginService.java
+++ 5. 프로그래밍 언어 활용/web_study/day02/src/test/java/models/member/LoginService.java
@@ -4,7 +4,9 @@ import jakarta.servlet.http.HttpServletRequest;
 
 public class LoginService {
     public void login(HttpServletRequest request) {
-
+        String userId = request.getParameter("userId");
+        String userPw = request.getParameter("userPw");
+        System.out.printf("userId=%s, userPw=%s%n",userId,userPw);
 
     }
 }
--- 5. 프로그래밍 언어 활용/web_study/day02/src/test/java/models/member/RequestLogin.java
+++ 5. 프로그래밍 언어 활용/web_study/day02/src/test/java/models/member/RequestLogin.java
@@ -1,5 +1,9 @@
 package models.member;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder @Getter
 public class RequestLogin {
     private String userId;
     private String userPw;
