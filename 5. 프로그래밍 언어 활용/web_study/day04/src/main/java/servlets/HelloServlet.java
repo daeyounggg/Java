a71ae777c154c480ec1c98ff01139ed11ac4f42c
daeyounggg <kdyzab@gmail.com>
2023-10-12 17:55:42 +0900

    a

--- 5. 프로그래밍 언어 활용/web_study/day04/src/main/java/servlets/HelloServlet.java
+++ 5. 프로그래밍 언어 활용/web_study/day04/src/main/java/servlets/HelloServlet.java
@@ -8,8 +8,20 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class HelloServlet extends HttpServlet {
+
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("안녕하세요.");
+        //System.out.println("안녕하세요.");
+        System.out.println("doGet!!!");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy!!!");
+    }
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("init!!!");
     }
 }
