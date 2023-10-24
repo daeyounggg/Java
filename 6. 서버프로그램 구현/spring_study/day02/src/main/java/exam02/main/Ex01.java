16d537530b20feb852cf051cb80dbb22f9d1c50a
daeyounggg <kdyzab@gmail.com>
2023-10-24 17:23:06 +0900

    수업 소스

--- 6. 서버프로그램 구현/spring_study/day02/src/main/java/exam02/main/Ex01.java
+++ 6. 서버프로그램 구현/spring_study/day02/src/main/java/exam02/main/Ex01.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Ex01 {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx2.class);
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx3.class);
 
         JoinService joinService = ctx.getBean("joinService", JoinService.class);
         InfoService infoService = ctx.getBean("infoService", InfoService.class);
