e71c4a230a7290ad1903e74f388f2ade5b8e3180
daeyounggg <kdyzab@gmail.com>
2023-10-24 17:17:59 +0900

    수업 소스

--- 6. 서버프로그램 구현/spring_study/day02/src/main/java/exam02/models/member/JoinService.java
+++ 6. 서버프로그램 구현/spring_study/day02/src/main/java/exam02/models/member/JoinService.java
@@ -2,7 +2,9 @@ package exam02.models.member;
 
 import exam02.commons.Validator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class JoinService {
 
     @Autowired
