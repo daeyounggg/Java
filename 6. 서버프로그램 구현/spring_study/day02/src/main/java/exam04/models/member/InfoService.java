66b6fb68d0dbacd6e6b51ff7b88340ecfa2f14db
daeyounggg <kdyzab@gmail.com>
2023-10-25 16:15:59 +0900

    수업 소스

---
+++ 6. 서버프로그램 구현/spring_study/day02/src/main/java/exam04/models/member/InfoService.java
@@ -0,0 +1,43 @@
+package exam04.models.member;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Service;
+
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class InfoService {
+
+
+    private final MemberDao memberDao;
+
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yy-MM-dd");
+
+    @Autowired
+    public void setFormatter(@Nullable DateTimeFormatter formatter){
+        System.out.println("유입?");
+        System.out.println(formatter);
+
+        this.formatter = formatter;
+    }
+
+    public void print(){
+        List<Member> members = memberDao.gets();
+        members.stream().map(this::toConvert).forEach(System.out::println);
+    }
+
+    private Member toConvert(Member member){
+        if(formatter == null){
+            return member;
+        }
+
+        String regDtStr = formatter.format(member.getRegDt());
+        member.setRegDtStr(regDtStr);
+
+        return member;
+    }
+}
