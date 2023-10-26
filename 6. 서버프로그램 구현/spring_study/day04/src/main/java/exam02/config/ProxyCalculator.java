9512c9f29ee42ce47d444191d58c67b8bee925c9
daeyounggg <kdyzab@gmail.com>
2023-10-26 16:26:01 +0900

    .

---
+++ 6. 서버프로그램 구현/spring_study/day04/src/main/java/exam02/config/ProxyCalculator.java
@@ -0,0 +1,29 @@
+package exam02.config;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class ProxyCalculator {
+
+    @Pointcut("execution(* exam02.aopex..*(..))")
+    public void publicTarget() {}
+
+    @Around("publicTarget()")
+    public Object process(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        long stime = System.nanoTime();
+
+        try {
+            Object result = joinPoint.proceed(); // factorial(..) - 핵심 기능(대신 수행)
+
+            return result;
+        }finally {
+            long etime = System.nanoTime();
+            System.out.printf("걸린 시간: %d%n", etime - stime);
+        }
+    }
+}
