3ed26fe89b7e7ae419befdd8988d62064e44a564
daeyounggg <kdyzab@gmail.com>
2023-10-26 17:55:28 +0900

    .

--- 6. 서버프로그램 구현/spring_study/day04/src/main/java/exam02/config/ProxyCalculator.java
+++ 6. 서버프로그램 구현/spring_study/day04/src/main/java/exam02/config/ProxyCalculator.java
@@ -5,14 +5,18 @@ import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
 
+@Order(2)
 @Aspect
 public class ProxyCalculator {
 
-    @Pointcut("execution(* exam02.aopex..*(..))")
-    public void publicTarget() {}
 
-    @Around("publicTarget()")
+
+
+    //@Around("publicTarget()")
+    // @Around("execution(* exam02.aopex..*(..))")
+    @Around("CommonPointcut.publicTarget()")
     public Object process(ProceedingJoinPoint joinPoint) throws Throwable {
 
         long stime = System.nanoTime();
